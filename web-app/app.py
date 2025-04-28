723cb15a64aa5b93995b33ad5dcdf48b303a855a
Zeklin <forrestwilliams@pureagave.org>
2025-04-28 13:18:54 -0400

    Initial Youtube API Integration

--- web-app/app.py
+++ web-app/app.py
@@ -4,20 +4,38 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from datetime import datetime
 from bson import ObjectId
+from collections import defaultdict
+from config import YOUTUBE_API_KEY
 import os
 import requests
 import json
 import re
+import isodate
 
 app = Flask(__name__)
 client = MongoClient("mongodb://mongodb:27017")
 db = client["youtube_history"]
 
-def processWatchHistory(raw_data, chunk_size=5000):
+metrics = {
+    "total_watchtime": 0,
+    "total_videos": 0,
+    "hourly_watchtime": {},
+    "tag_frequency": {},
+    "channel_stats": defaultdict(lambda: {"watchtime": 0, "frequency": 0}),
+    "category_stats": defaultdict(lambda: {"watchtime": 0, "frequency": 0}),
+    "longest_video": {"video_id": "", "duration": 0},
+    "shortest_video": {"video_id": "", "duration": float("inf")},
+}
+
+def processWatchHistory(raw_data, chunk_size=5000, limit=1000):
     clean_data = []
+    video_count = 0
     with open(raw_data, "r", encoding="utf-8") as f:
         records = json.load(f)
         for record in records:
+            if video_count >= limit:
+                break
+            
             if "titleUrl" not in record:
                 continue
 
@@ -34,16 +52,92 @@ def processWatchHistory(raw_data, chunk_size=5000):
                 "timestamp": timestamp
             })
             
+            video_count += 1
+            
             if len(clean_data) >= chunk_size:
-                saveWatchHistory(clean_data)
+                enrichData(clean_data)
+                logOutput()
                 clean_data = []
                 
         if clean_data:
-            saveWatchHistory(clean_data)
+            enrichData(clean_data)
+            logOutput()
 
-def saveWatchHistory(clean_chunk):
+def saveWatchHistory(clean_chunk, last_chunk=False):
     #save to mongodb
     return
+
+def enrichData(clean_chunk):
+    subchunk = []
+    # collect 50 video ids to send to youtube
+    for video in clean_chunk:
+        # process hourly watchtime
+        temp_time = video["timestamp"].hour - 4
+        if temp_time < 0:
+            temp_time += 24
+        metrics["hourly_watchtime"][temp_time] = metrics["hourly_watchtime"].get(temp_time, 0) + 1
+        
+        subchunk.append(video["video_id"])
+        
+        if len(subchunk) == 50:
+            api_ids = ",".join(subchunk)
+            # api call
+            params = {
+                "key": YOUTUBE_API_KEY,
+                "part": "snippet,contentDetails,statistics",
+                "fields": "items(id,snippet(title,channelTitle,categoryId,tags,publishedAt),contentDetails(duration))",
+                "id": api_ids,
+            }
+            response = requests.get("https://www.googleapis.com/youtube/v3/videos", params=params)
+            enriched_video_data = response.json()
+            
+            for enriched_video in enriched_video_data["items"]:
+                # temp datastore for easy access
+                temp_duration = isodate.parse_duration(enriched_video["contentDetails"]["duration"]).total_seconds()
+                # 6 hour cap
+                if temp_duration > 21600:
+                    continue
+                temp_tags = enriched_video["snippet"].get("tags", [])
+                temp_channel = enriched_video["snippet"].get("channelTitle", "UnknownChannel")
+                temp_category = enriched_video["snippet"].get("categoryId", "UnknownCategory")
+                
+                # metrics update
+                metrics["total_watchtime"] += temp_duration
+                metrics["total_videos"] += 1
+                for tag in temp_tags:
+                    metrics["tag_frequency"][tag] = metrics["tag_frequency"].get(tag, 0) + 1
+                metrics["channel_stats"][temp_channel]["watchtime"] += temp_duration
+                metrics["channel_stats"][temp_channel]["frequency"] += 1
+                metrics["category_stats"][temp_category]["watchtime"] += temp_duration
+                metrics["category_stats"][temp_category]["frequency"] += 1
+                if temp_duration > metrics["longest_video"]["duration"]:
+                    metrics["longest_video"]["video_id"] = enriched_video["id"]
+                    metrics["longest_video"]["duration"] = temp_duration
+                if temp_duration < metrics["shortest_video"]["duration"]:
+                    metrics["shortest_video"]["video_id"] = enriched_video["id"]
+                    metrics["shortest_video"]["duration"] = temp_duration
+            subchunk = []
+    # TODO still need to process last subchunk
+    return
+
+def logOutput():
+    print("Total Watchtime: ", metrics["total_watchtime"])
+    print("Total Videos: ", metrics["total_videos"])
+    print("Hourly Watchtime: ", metrics["hourly_watchtime"])
+    print("\nTag Frequency: ")
+    sorted_tags = sorted(metrics["tag_frequency"].items(), key=lambda x: x[1], reverse=True)[:10]
+    for tag, freq in sorted_tags:
+        print(f"{tag}: {freq}")
+    print("\nChannel Stats: ")
+    sorted_channels = sorted(metrics["channel_stats"].items(), key=lambda x: x[1]["frequency"], reverse=True)[:10]
+    for channel, freq in sorted_channels:
+        print(f"{channel}: {freq}")
+    print("\nCategory Stats: ")
+    sorted_categories = sorted(metrics["category_stats"].items(), key=lambda x: x[1]["frequency"], reverse=True)[:10]
+    for category, freq in sorted_categories:
+        print(f"{category}: {freq}")
+    print("\nLongest Video: ", '"', metrics["longest_video"]["video_id"], '" ', '"', metrics["longest_video"]["duration"], '"')
+    print("Shortest Video: ", '"', metrics["shortest_video"]["video_id"], '" ', '"', metrics["shortest_video"]["duration"], '"')
 
 @app.route("/")
 def home():
